c932a9511128ad5f936e90b15fc54c72e17ef54e
nhustonlyons <nhustonlyons@gmail.com>
2017-06-21 22:59:12 -0400

    Add files via upload

--- Customer-Support-v7/source/production/java/com/wrox/DBManager.java
+++ Customer-Support-v7/source/production/java/com/wrox/DBManager.java
@@ -6,12 +6,13 @@ import java.util.*;
 public class DBManager {
 	private Connection myConn;
 	private Statement myStat;
+	private int nextRef;
 	
 	public DBManager(){
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
 			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ticket", "ITAdmin", "ticketDB");
-			
+			nextRef = 0;
 			myStat = myConn.createStatement();
 		}
 		catch(Exception exc){
@@ -21,6 +22,7 @@ public class DBManager {
 	public Map<Integer, Ticket> pullAllTickets() {
 		//Initializing Return Map
 		Map<Integer, Ticket> returnThis = new LinkedHashMap<>();
+		int max = 0;
 		try{
 		
 		//Statement Prep
@@ -42,9 +44,12 @@ public class DBManager {
 			tempattach.setName("placeholder");
 			tempattach.setContents(myRs.getBytes("Attachment"));
 			temp.addAttachment(tempattach);
+			int tempInt = myRs.getInt("RefID");
 			
-			
-			returnThis.put(myRs.getInt("RefID"), temp);
+			if(tempInt > nextRef){
+				nextRef = tempInt;
+			}
+			returnThis.put(tempInt, temp);
 			}
 		//End of Try
 		}
@@ -64,7 +69,7 @@ public class DBManager {
         try {
             ps = connection.prepareStatement(query);
             ps.setString(4, ticket.getBody());
-            ps.setInt(2, rando.nextInt(9000000));
+            ps.setInt(1, ticket.getRefID());
             java.sql.Date sqlDate = null;
             if(ticket.getDateCreated()!=null){
             	sqlDate = new java.sql.Date(ticket.getDateCreated().getTime());
@@ -75,7 +80,7 @@ public class DBManager {
             }
             ps.setDate(5, sqlDate); //Instance vs Date issue
             ps.setString(3, ticket.getSubject());	
-            ps.setInt(1, rando.nextInt(9000000));
+            ps.setInt(2, 2);
             //ps.setDate(6, sqlDateClosed);
             //Again figure out attachment issue
             ps.executeUpdate();
@@ -86,7 +91,8 @@ public class DBManager {
         }
     }
 	
-	public void createUser(int UserID, String Username, String pass, String First, String Last, java.sql.Date Date_Created, java.sql.Date Last_Session){
+	
+	public void createUser(int UserID, String Username, String pass, String First, String Last){
 		PreparedStatement ps = null;
 		
 		String query
@@ -100,8 +106,9 @@ public class DBManager {
 			ps.setString(3, pass);
 			ps.setString(4, First);
 			ps.setString(5, Last);
-			ps.setDate(6, Date_Created);
-			ps.setDate(7, Last_Session);
+			java.sql.Date sqlDate = new Java.sql.Date(utilDate.getTime());
+			ps.setDate(6, sqlDate);
+			ps.setDate(7, sqlDate);
 			ps.executeUpdate();
 		} catch (SQLException e){
 			System.out.println(e);			
